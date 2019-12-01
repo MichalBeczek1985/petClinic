13520f745a2a9f60da69ca8cdcfa65c24a1b6153
MichalBeczek1985 <31369080+MichalBeczek1985@users.noreply.github.com>
2019-12-01 12:33:24 +0100

    adding dataloader

--- pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package petclinic.services.map;
 
 import petclinic.model.Owner;
-import petclinic.services.CrudService;
+import petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner save(Owner object) {
         return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findByLastName(String lastname) {
+        return null;
     }
 }
