8a52ff347cff81108d720610a00c2b1a5f10ea37
MichalBeczek1985 <31369080+MichalBeczek1985@users.noreply.github.com>
2019-12-01 11:28:40 +0100

    implement mapservices

---
+++ pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package petclinic.services.map;
+
+import petclinic.model.Owner;
+import petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+}
